bd368919b8900a2cc65dc3ef915ecce84f7c3e2d
Tijmen Brommet <tijmen@gmail.com>
2015-10-26 10:12:34 +0000

    Send errors to Airbrake
    On GOV.UK we mostly use Airbrake. If it is defined, we can send the
    error there.

--- lib/govuk_message_queue_consumer/json_processor.rb
+++ lib/govuk_message_queue_consumer/json_processor.rb
@@ -7,7 +7,8 @@ module GovukMessageQueueConsumer
     def process(message)
       message.payload = JSON.parse(message.payload)
       @next_processor.process(message)
-    rescue JSON::ParserError
+    rescue JSON::ParserError => e
+      Airbrake.notify_or_ignore(e) if defined?(Airbrake)
       message.discard
     end
   end
