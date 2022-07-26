5f8343a9f467b1ac998ac7e810f9b5408eab0f2a
Kevin Dew <kevindew@me.com>
2022-07-26 19:48:10 +0100

    Resolve RSpec/LeakyConstantDeclaration
    This resolves a case of a constant defined in a block and changes this
    to use an anonymous class

--- spec/test_helpers/shared_examples_spec.rb
+++ spec/test_helpers/shared_examples_spec.rb
@@ -2,11 +2,13 @@ require_relative "../spec_helper"
 require "govuk_message_queue_consumer/test_helpers"
 
 describe "The usage of the shared example" do
-  class WellDevelopedMessageQueueConsumer
-    def process(_message); end
+  subject do
+    klass = Class.new do
+      def process(_message); end
+    end
+
+    klass.new
   end
 
-  describe WellDevelopedMessageQueueConsumer do
-    it_behaves_like "a message queue processor"
-  end
+  it_behaves_like "a message queue processor"
 end
